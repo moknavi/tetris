ad4c15fdbb38c22d07f9477ee952852c2dc235e2
moknavi <komazec.ivan@gmail.com>
2023-10-01 20:28:52 +0200

    Finished off work for today.

--- tetris.py
+++ tetris.py
@@ -8,9 +8,10 @@ gui_surface = pygame.display.set_mode(size = (220, 440))
 game_surface = pygame.display.set_mode(size = (200, 400))
 
 def draw_gui(score):
-    score_text = pygame.font.Font.render(f"Score: {score}".encode(), False, (255, 255, 255))
+    score_text = pygame.font.Font.render(pygame.font.Font(), f"Score: {score}".encode(), False, (255, 255, 255))
     gui_surface.blit(score_text, (0,0))
     pygame.display.flip()
 
+#Game loop
 while True:
     draw_gui(score)
